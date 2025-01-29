d60c5d35269844486f83c505a322cd771bd819c8
Debiprasad <debiprasadsahoo987@gmail.com>
2025-01-29 13:38:00 +0530

    few changes

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -13,7 +13,7 @@ public class UserBookingService {
     private User user;
     private List<User> userList;
     private ObjectMapper objectMapper = new ObjectMapper();
-    private static final String USERS_PATH = "../localDb/users.json";
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
     public UserBookingService(User user) throws IOException {
         this.user=user;
         File users = new File(USERS_PATH);
